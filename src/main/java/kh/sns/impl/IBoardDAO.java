43adae2a33e76b08834e4e8092a577a99fe85c1e
Unknown <yoonbumtae@gmail.com>
2018-08-07 12:23:59 +0900

    글쓰기
    일부 기능 구현

--- src/main/java/kh/sns/impl/IBoardDAO.java
+++ src/main/java/kh/sns/impl/IBoardDAO.java
@@ -10,6 +10,9 @@ import kh.sns.interfaces.BoardDAO;
 
 @Repository
 public class IBoardDAO implements BoardDAO  {
+	
+	private static final String SAVE_PATH = "/upload";
+	private static final String PREFIX_URL = "/upload/";
 
 	@Autowired
 	private JdbcTemplate template;
@@ -21,10 +24,18 @@ public class IBoardDAO implements BoardDAO  {
 		return template.update(sql, board.getContents(), board.getId());
 	}
 	
+	/*
+	 * (non-Javadoc)
+	 * @see kh.sns.interfaces.BoardDAO#insertNewMedia(kh.sns.dto.Board_MediaDTO)
+	 * 
+	 * 편의상 여기다 일단 만들고 나중에 필요하면 별도 클래스로 분리합니다.
+	 */
+	
 	@Override
 	public int insertNewMedia(Board_MediaDTO media) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		String sql = "insert into board_media values(board_media_seq.nextval, 2, ?, ?, ?)";
+		return template.update(sql, media.getMedia_type(), media.getOriginal_file_name(), media.getSystem_file_name());
 	}
 
 }
--- src/main/java/kh/sns/impl/IBoardService.java
+++ src/main/java/kh/sns/impl/IBoardService.java
@@ -1,17 +1,27 @@
 package kh.sns.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import kh.sns.dto.BoardDTO;
 import kh.sns.dto.Board_MediaDTO;
+import kh.sns.interfaces.BoardDAO;
 import kh.sns.interfaces.BoardService;
 
 @Service
 public class IBoardService implements BoardService {
 	
+	@Autowired
+	BoardDAO bdao;
+	
 	@Override
+	// @Transactional("txManager")
 	public int insertNewArticle(BoardDTO boardContent, Board_MediaDTO boardMedia) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		int contentResult = bdao.insertNewBoardContent(boardContent);
+		int mediaResult = bdao.insertNewMedia(boardMedia);
+		
+		return contentResult * mediaResult;
 	}
 }
