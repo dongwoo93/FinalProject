613d2b12074e611788141a62578800afd21ebe0e
Unknown <hyong0701@gmail.com>
2018-08-14 09:56:59 +0900

    commit

--- src/main/java/kh/sns/controller/Board_CommentController.java
+++ src/main/java/kh/sns/controller/Board_CommentController.java
@@ -14,50 +14,58 @@ import kh.sns.interfaces.Board_CommentService;
 @Controller
 public class Board_CommentController {
 
-	@Autowired
-	private Board_CommentService boardcommentservice;
+   @Autowired
+   private Board_CommentService boardcommentservice;
 
 
-	@RequestMapping("/comment.co")
-	public void insertComment(Board_CommentDTO dto, HttpSession session, HttpServletRequest response) {
-		String id = (String)session.getAttribute("loginId");
-		dto.setId(id);
-		int result = 0;
-		try {
-			result =this.boardcommentservice.insertComment(dto);
-			
-			if(result >0) { 
-				System.out.println("success");
-			}
-			else {  
-				System.out.println("failed");  
-			}
+   @RequestMapping("/comment.co")
+   public void insertComment(Board_CommentDTO dto, HttpSession session, HttpServletResponse response) {
+      System.out.println(dto.getComment_seq());
+      String id = (String)session.getAttribute("loginId");
+      dto.setId(id);
+      int result = 0;
+      int commentseq = 0;
+      try { 
+         commentseq = this.boardcommentservice.getCommentSeq();
+         dto.setComment_seq(commentseq);
+         result =this.boardcommentservice.insertComment(dto);
+         
+         if(result >0) {  
+            System.out.println("success");
+            System.out.println(dto.getComment_seq()); 
+            response.getWriter().print(dto.getComment_seq());
+            response.getWriter().flush();
+            response.getWriter().close();
+         }
+         else {  
+            System.out.println("failed");  
+         }
 
-		}catch(Exception e) {
-			System.out.println("요기는 comment.co입니다");
-			e.printStackTrace();
-		}
-	
-	}
-	
-	@RequestMapping("/commentdel.co")
-	public void delComment(String comment_seq ,HttpServletResponse response)  {
-		System.out.println(comment_seq);  
-		int result = 0; 
-		try {
-			result = this.boardcommentservice.delComment(Integer.parseInt(comment_seq));   
-			System.out.println(result);   
-			if(result > 0 ) {
-				System.out.println("del success");	
-			}	
-			else {
-				System.out.println("del failed");
-			}
-			
-		}catch(Exception e) {
-			System.out.println("요기는  commentdel.co입니다");
-			e.printStackTrace();
-		}
-	  
-	}
+      }catch(Exception e) {
+         System.out.println("요기는 comment.co입니다");
+         e.printStackTrace();
+      }
+   
+   }
+   
+   @RequestMapping("/commentdel.co")
+   public void delComment(String comment_seq ,HttpServletResponse response)  {
+      System.out.println(comment_seq);  
+      int result = 0; 
+      try {
+         result = this.boardcommentservice.delComment(Integer.parseInt(comment_seq));   
+         System.out.println(result);   
+         if(result > 0 ) {
+            System.out.println("del success");   
+         }   
+         else {
+            System.out.println("del failed");
+         }
+         
+      }catch(Exception e) {
+         System.out.println("요기는  commentdel.co입니다");
+         e.printStackTrace();
+      }
+     
+   }
 }
--- src/main/java/kh/sns/impl/IBoard_CommentDAO.java
+++ src/main/java/kh/sns/impl/IBoard_CommentDAO.java
@@ -1,11 +1,14 @@
 package kh.sns.impl;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import kh.sns.dto.Board_CommentDTO;
@@ -13,17 +16,17 @@ import kh.sns.interfaces.Board_CommentDAO;
 
 @Repository
 public class IBoard_CommentDAO implements Board_CommentDAO {
-	
+
 	@Autowired
 	private JdbcTemplate template;
-	
+
 	@Autowired
 	private SqlSessionTemplate sqltemplate;
 
 	@Override
 	public int insertComment(Board_CommentDTO dto) throws Exception {
 		String sql = "insert into board_comment values(board_comment_seq.nextval,?,?,?,default)";
-System.out.println(dto.getComment_contents() + " : " + dto.getId() + " : " + dto.getBoard_seq()); 
+		System.out.println(dto.getComment_contents() + " : " + dto.getId() + " : " + dto.getBoard_seq()); 
 		return template.update(sql, dto.getBoard_seq(),dto.getId(),dto.getComment_contents());
 	}
 
@@ -38,9 +41,20 @@ System.out.println(dto.getComment_contents() + " : " + dto.getId() + " : " + dto
 		String sql = "delete from board_comment where comment_seq=?";
 		return template.update(sql,comment_seq);
 	}
-	
-	
-	
 
+	@Override
+	public int getCommentSeq() throws Exception {
+		String sql = "select BOARD_COMMENT_SEQ.nextval from dual";
+		List<Integer> list = template.query(sql, new RowMapper<Integer>() {
+
+			@Override
+			public Integer mapRow(ResultSet rs, int rowNum) throws SQLException {
+				return rs.getInt("nextval");
+			}
+		});
+ 
+		System.out.println(list.get(0));
+		return list.get(0);
+	}
 
 }
